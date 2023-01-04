3f34446a2b869906780aead03b242c14568be7c9
Pedro Laburo <juan.pedro.casanas99@gmail.com>
2023-01-03 21:41:40 -0300

    Left at chapter 5 project 6

--- C Programming - A Modern Approach/Chapter 5/projects/4/4.c
+++ C Programming - A Modern Approach/Chapter 5/projects/4/4.c
@@ -1,7 +1,33 @@
 #include <stdio.h> 
  
 int main (void) 
-{ 
+{
+   float windSpeed;
+
+   printf("Please enter the windspeed in knots: \n");
+   scanf("%f", &windSpeed);
+
+   if (windSpeed >= 0){
+      windSpeed = windSpeed;
+   } else {
+      windSpeed = -windSpeed;
+   }
+
+   printf("Speed (Knots)\tDescription\n");
+
+   if(windSpeed > 63) {
+      printf("%.2f\t\tHurricane", windSpeed);
+   } else if (windSpeed >= 48){
+      printf("%.2f\t\tStorm", windSpeed);
+   } else if (windSpeed >= 28){
+      printf("%.2f\t\tGale", windSpeed);
+   } else if (windSpeed >= 4){
+      printf("%.2f\t\tBreeze", windSpeed);
+   } else if (windSpeed >= 1){
+      printf("%.2f\t\tLight Air", windSpeed);
+   } else {
+      printf("%.2f\t\tCalm", windSpeed);
+   }
  
    return 0; 
 } 
--- C Programming - A Modern Approach/Chapter 5/projects/5/5.c
+++ C Programming - A Modern Approach/Chapter 5/projects/5/5.c
@@ -2,6 +2,37 @@
  
 int main (void) 
 { 
+   float income;
+   float amountOver;
+   float tax;
+   float total;
+   
+   printf("Enter an ammount of taxable income: \n");
+   scanf("%f", &income);
+
+   if (income > 7000){
+      amountOver = income - 7000;
+      tax = 230.0f + 0.06 * amountOver;
+   } else if (income > 5250){
+      amountOver = income - 5250;
+      tax = 142.50f + 0.05 * amountOver;
+   } else if (income > 3750){
+      amountOver = income - 3750;
+      tax = 82.50f + 0.04 * amountOver;
+   } else if (income > 2250){
+      amountOver = income - 2250;
+      tax = 37.50f + 0.03 * amountOver;
+   } else if (income > 750){
+      amountOver = income - 750;
+      tax = 7.50f + 0.02 * amountOver;
+   } else {
+      tax = 0.01 * income;
+   }
+
+   total = income - tax;
+
+   printf("Income\tTax\tTotal\n");
+   printf("%.2f\t%.2f\t%.2f", income, tax, total);
  
    return 0; 
 } 
