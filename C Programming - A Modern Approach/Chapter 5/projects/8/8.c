4bf18c4112592261879c6a905a54c6c420d6ba48
Pedro Laburo <juan.pedro.casanas99@gmail.com>
2023-01-05 11:54:21 -0300

    Finished up until the ninth chapter 5 project

--- C Programming - A Modern Approach/Chapter 5/projects/8/8.c
+++ C Programming - A Modern Approach/Chapter 5/projects/8/8.c
@@ -4,45 +4,119 @@ int main (void)
 {
    int hours;
    int minutes;
-   int ampm;
-   
-   int closestDHour;
-   int closestDMinute;
-   int closestAHour;
-   int closestAMinute;
+   int dTime1;
+   int dTime2;
+   int aTime1;
+   int aTime2;
+   int dif1;
+   int dif2;
+   int totalTime;
+   int dHours;
+   int dMinutes;
+   int aHours;
+   int aMinutes;
 
    printf("Enter a time in 24-Hour format: \n");
    scanf("%2d:%2d", &hours, &minutes);
 
-   if ((hours > 12) || (hours == 12 && minutes > 0)){
-      ampm = 1;
-   } else {
-      ampm = 0;
+   totalTime = (hours * 60) + minutes;
+
+   dTime1 = (8 * 60);
+   aTime1 = (10 * 60) + 16;
+
+   dTime2 = (9 * 60) + 43;
+   aTime2 = 11 * 60 + 52;
+
+   dif1 = dTime1 - totalTime;
+   dif2 = dTime2 - totalTime;
+
+   if ((dif1 > dif2 && dif2 >= 0) || dif1 < 0){
+      dTime1 = dTime2;
+      aTime1 = aTime2;
    }
 
-   switch(hours){
-      case 13: hours = 1; break;
-      case 14: hours = 2; break;
-      case 15: hours = 3; break;
-      case 16: hours = 4; break;
-      case 17: hours = 5; break;
-      case 18: hours = 6; break;
-      case 19: hours = 7; break;
-      case 20: hours = 8; break;
-      case 21: hours = 9; break;
-      case 22: hours = 10; break;
-      case 23: hours = 11; break;
-      case 24: hours = 12; break;
+   dTime2 = (11 * 60) + 19;
+   aTime2 = (13 * 60) + 31;
+
+   dif1 = dTime1 - totalTime;
+   dif2 = dTime2 - totalTime;
+
+   if ((dif1 > dif2 && dif2 >= 0) || dif1 < 0){
+      dTime1 = dTime2;
+      aTime1 = aTime2;
    }
 
-   if (ampm == 0){
-      if (hours < 9) {
-         closestDHour = 8;
-         closestDMinute = 00;
-         closestAHour = 10;
-         closestAMinute = 
-      }
+   dTime2 = (12 * 60) + 47;
+   aTime2 = (15 * 60);
+
+   dif1 = dTime1 - totalTime;
+   dif2 = dTime2 - totalTime;
+
+   if ((dif1 > dif2 && dif2 >= 0) || dif1 < 0){
+      dTime1 = dTime2;
+      aTime1 = aTime2;
+   }
+
+   dTime2 = (14 * 60);
+   aTime2 = (16 * 60) + 8;
+
+   dif1 = dTime1 - totalTime;
+   dif2 = dTime2 - totalTime;
+
+   if ((dif1 > dif2 && dif2 >= 0) || dif1 < 0){
+      dTime1 = dTime2;
+      aTime1 = aTime2;
+   }
+
+   dTime2 = (15 * 60) + 45;
+   aTime2 = (17 * 60) + 55;
+
+   dif1 = dTime1 - totalTime;
+   dif2 = dTime2 - totalTime;
+
+   if ((dif1 > dif2 && dif2 >= 0) || dif1 < 0){
+      dTime1 = dTime2;
+      aTime1 = aTime2;
+   }
+
+   dTime2 = (19 * 60);
+   aTime2 = (21 * 60) + 20;
+
+   dif1 = dTime1 - totalTime;
+   dif2 = dTime2 - totalTime;
+
+   if ((dif1 > dif2 && dif2 >= 0) || dif1 < 0){
+      dTime1 = dTime2;
+      aTime1 = aTime2;
    }
  
+   dTime2 = (21 * 60) + 45;
+   aTime2 = (23 * 60) + 58;
+
+   dif1 = dTime1 - totalTime;
+   dif2 = dTime2 - totalTime;
+
+   if ((dif1 > dif2 && dif2 >= 0) || dif1 < 0){
+      dTime1 = dTime2;
+      aTime1 = aTime2;
+   }
+
+   dHours = dTime1 / 60;
+   dMinutes = dTime1 % 60;
+   aHours = aTime1 / 60;
+   aMinutes = aTime1 % 60;
+
+   if (dHours >= 12){
+      printf("The closest departure time is: %.2d:%.2d PM\n", dHours - 12, dMinutes);
+   } else {
+      printf("The closest departure time is: %.2d:%.2d AM\n", dHours, dMinutes);
+   }
+
+   if (aHours >= 12){
+      printf("The closest departure time is: %.2d:%.2d PM\n", aHours - 12, aMinutes);
+   } else {
+      printf("The closest departure time is: %.2d:%.2d AM\n", aHours, aMinutes);
+   }
+
    return 0; 
 } 
--- C Programming - A Modern Approach/Chapter 5/projects/9/9.c
+++ C Programming - A Modern Approach/Chapter 5/projects/9/9.c
@@ -1,7 +1,53 @@
 #include <stdio.h> 
  
 int main (void) 
-{ 
+{
+   int dd1;
+   int mm1;
+   int yy1;
+   int dd2;
+   int mm2;
+   int yy2;
+   int earliestDate; 
  
+   printf("Enter first date: \n");
+   scanf("%2d/%2d/%2d", &dd1, &mm1, &yy1);
+
+   printf("Enter second date: \n");
+   scanf("%2d/%2d/%2d", &dd2, &mm2, &yy2);
+
+   if (yy1 != yy2){
+      if(yy1 < yy2){
+         earliestDate = 0;
+      } else {
+         earliestDate = 1;
+      }
+   } else if (mm1 != mm2){
+      if (mm1 < mm2){
+         earliestDate = 0;
+      } else {
+         earliestDate = 1;
+      }
+   } else if (dd1 != dd2) {
+      if (dd1 < dd2){
+         earliestDate = 0;
+      } else {
+         earliestDate = 1;
+      }
+   } else {
+      earliestDate = -1;
+   }
+
+   if(earliestDate != -1){
+      printf("The earliest date between %.2d/%.2d/%.2d and %.2d/%.2d/%.2d is: ", dd1, mm1, yy1, dd2, mm2, yy2);
+      if (earliestDate == 0){
+         printf("%.2d/%.2d/%.2d\n", dd1, mm1, yy1);
+      } else {
+         printf("%.2d/%.2d/%.2d\n", dd2, mm2, yy2);
+      }
+   } else {
+      printf("%.2d/%.2d/%.2d and %.2d/%.2d/%.2d are equal dates.\n", dd1, mm1, yy1, dd2, mm2, yy2);
+   }
+
    return 0; 
 } 
